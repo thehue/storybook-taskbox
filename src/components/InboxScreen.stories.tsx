f475edf70ca5491683d0e56f5971964a388ac327
hy.jang <hy.jang@autowini.com>
2021-08-25 10:17:42 +0900

    Supplying context with decorators

--- src/components/InboxScreen.stories.tsx
+++ src/components/InboxScreen.stories.tsx
@@ -1,10 +1,32 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
+import { Provider } from "react-redux";
+
 import { PureInboxScreen } from "./InboxScreen";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import * as TaskListStories from "./TaskList.stories";
+import { TaskProps } from "./Task";
+import { createStore } from "redux";
+
+type Tasks = {
+  tasks: TaskProps["task"][];
+};
+
+const TaskListStoriesDefaultArgs = TaskListStories.Default.args as Tasks;
+const initialState = {
+  tasks: TaskListStoriesDefaultArgs.tasks,
+};
+// A super-simple mock of a redux store
+
+const reducer = (state: Tasks = initialState) => {
+  return state;
+};
+
+const store = createStore(reducer);
 
 export default {
   title: "InboxScreen",
   component: PureInboxScreen,
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 } as ComponentMeta<typeof PureInboxScreen>;
 
 const Template: ComponentStory<typeof PureInboxScreen> = (args) => (
